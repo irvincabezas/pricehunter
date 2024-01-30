c72ef6a957995ca5ebd73e5f55a485d005bbfddf
irvincabezas <irvin.cabezasmartinez@telus.com>
2024-01-30 02:47:43 -0600

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
-import type { Config } from "tailwindcss";
+import config from "./node_modules/next/config";
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,10 +9,36 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          DEFAULT: "#1F305E", // Dark Blue
+          orange: "#FFA500", // Orange
+          green: "#008000", // Green
+        },
+        secondary: "#282828", // Secondary (Dark Grey)
+        "gray-200": "#EAECF0", // Light Grey
+        "gray-300": "#D0D5DD", // Medium Grey
+        "gray-500": "#667085", // Slate Grey
+        "gray-600": "#475467", // Steel Grey
+        "gray-700": "#344054", // Charcoal Grey
+        "gray-900": "#101828", // Deep Grey
+        "white-100": "#F4F4F4", // Light White
+        "white-200": "#EDF0F8", // Soft White
+        "black-100": "#3D4258", // Soft Black
+        "neutral-black": "#23263B", // Neutral Black
+      },
+      boxShadow: {
+        xs: "0px 1px 2px 0px rgba(16, 24, 40, 0.05)",
+      },
+      maxWidth: {
+        "10xl": "1440px",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        spaceGrotesk: ["Space Grotesk", "sans-serif"],
+      },
+      borderRadius: {
+        10: "10px",
       },
     },
   },
